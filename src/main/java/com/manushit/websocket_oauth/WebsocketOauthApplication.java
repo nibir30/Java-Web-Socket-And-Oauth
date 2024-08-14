eede3ca3cf75a35d9856cf0734f203560ae61ae7
Nibir Rahman <r.nibir30@gmail.com>
2024-08-14 14:37:38 +0600

    java web socket initializer

---
+++ src/main/java/com/manushit/websocket_oauth/WebsocketOauthApplication.java
@@ -0,0 +1,13 @@
+package com.manushit.websocket_oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manushit/websocket_oauth/WebsocketOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manushit.websocket_oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
